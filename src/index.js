406aaccb5b45950685681fba220d96f312df09f9
Atishek Kumar <kumaratishek22@gmail.com>
2021-06-19 15:13:35 +0530

    Added the apollo client

--- src/index.js
+++ src/index.js
@@ -2,10 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink
+} from "@apollo/client";
+
+import { setContext } from "apollo-link-context";
+
+const uri = process.env.API_URI || 'http://localhost:4000/api';
+
+const httpLink = createHttpLink({uri});
+const cache = new InMemoryCache();
+
+const authLink = setContext((_, {headers}) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('token') || ''
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache,
+  resolvers: {},
+  connectToDevTools: true
+});
+
+//const data = {
+//  loggedIn: !!localStorage.getItem('token')
+//}
+//
+//cache.modify({data});
+//
+//client.resetStore(() => cache.modify({data}));
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
